5e53b0ce5c0e32314c26edae22e280e8d7bd273c
Jason Deal <jmd613@outlook.com>
2017-01-28 14:35:03 -0500

    Updates

--- ProtoBoard2017/Program.cs
+++ ProtoBoard2017/Program.cs
@@ -11,15 +11,51 @@ namespace ProtoBoard2017
         public static void Main()
         {
             TalonSrx talon = new TalonSrx(1);
+            TalonSrx talon2 = new TalonSrx(2);
             OI controller = new OI(new Gamepad(new UsbHostDevice()));
-            float motorSpeed;
-            while (controller.GetConnectionStatus())
+            float motorSpeed = .70f;
+            float motorStoredSpeed = motorSpeed;
+            bool isPressed = false;
+            while (true)
             {
-                motorSpeed = controller.GetAxis(OI.Axis.left_x);
-                motorSpeed = controller.InvertMotorSpeed(motorSpeed);
+                if(controller.GetButton(OI.Button.a) && !isPressed)
+                {
+                    motorSpeed += 0.05f;
+                } else if (controller.GetButton(OI.Button.b) && !isPressed)
+                {
+                    motorSpeed -= 0.05f;
+                }
+
+                if(controller.GetButton(OI.Button.x) && !isPressed)
+                {
+                    motorSpeed += 0.01f;
+                } else if (controller.GetButton(OI.Button.y) && !isPressed)
+                {
+                    motorSpeed -= 0.01f;
+                }
+
+                if (controller.GetButton(OI.Button.a) || controller.GetButton(OI.Button.b) || controller.GetButton(OI.Button.x) || controller.GetButton(OI.Button.y))
+                {
+                    isPressed = true;
+                }
+                else
+                {
+                    isPressed = false;
+                }
+
+                if (controller.GetAxis(OI.Axis.left_y) == -1)
+                {
+                    motorSpeed = 0;
+                } else if (controller.GetAxis(OI.Axis.left_y) == 1)
+                {
+                    motorSpeed = motorStoredSpeed;
+                }
+
                 talon.Set(motorSpeed);
+                talon2.Set(motorSpeed);
                 CTRE.Watchdog.Feed();
-                Debug.Print("Power: " + talon.Get().ToString());
+                Debug.Print("Motor Speed = " + motorSpeed);
+
             }
         }
     }
